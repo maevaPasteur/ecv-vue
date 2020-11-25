0a75770cd02003e04bac0c70b978e4a1bf171ca4
luctst <lucas.tostee@gmail.com>
2020-11-25 03:54:51 +0100

    Create isAdmin middleware

---
+++ api/middlewares/isAdmin.js
@@ -0,0 +1,20 @@
+const UsersModel = require('../db/models/Users');
+
+const response = require('../utils/response');
+
+/**
+ * Check if user is an admin
+ */
+module.exports = async function (req, res, next) {
+    const { role } = await UsersModel.findById(res.locals.data.newSession.userId, 'role').exec();
+    console.log(role);
+
+    if (role) {
+        return next();
+    } else {
+        response(res, 401, {
+            serverHeader: res.locals.data.header  ? { ...res.locals.data.header } : undefined,
+            token: res.locals.data.forClient ? res.locals.data.forClient.token : undefined
+        });
+    }
+}
