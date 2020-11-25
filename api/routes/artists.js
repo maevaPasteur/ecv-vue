a3b67848e514e231a4c4b1311f17234136fee617
luctst <lucas.tostee@gmail.com>
2020-11-25 03:55:54 +0100

    Create /like/:id route for api

--- api/routes/artists.js
+++ api/routes/artists.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
+const {likeArtistById} = require('../db/artists');
+const isConnected = require('../middlewares/isConnected');
+const response = require('../utils/response');
 const Artists = require('../db/models/Artists');
 
 router.get('/', async (req, res) => {
@@ -28,6 +32,29 @@ router.patch('/:id', async (req, res) => {
     } catch (err) {
         res.json({message: err})
     }
+});
+
+router.patch('/like/:id', isConnected, async (req, res) => {
+    const b = Object.keys(req.body);
+
+    if (b.length > 1) {
+        return response(res, 422);
+    }
+
+    if (!b.includes('shouldLiked')) {
+        return response(res, 422);
+    }
+
+    const responseController = await likeArtistById(req.params.id, res.locals.data.newSession.userId, req.body.shouldLiked);
+
+    return response(res, responseController.code, {
+        serverHeader: res.locals.data.header || responseController.serverHeader ? { 
+            ...res.locals.data.header,
+            ...responseController.serverHeader
+        } : undefined,
+        modifyResponse: responseController.modifyResponse ? { ...responseController.modifyResponse} : undefined,
+        token: res.locals.data.forClient ? res.locals.data.forClient.token : undefined
+    })
 });
 
 router.delete('/:id', async (req, res) => {
