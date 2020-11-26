5e71425f733f41ab57c4e57c85a7fe2c93e20d76
luctst <lucas.tostee@gmail.com>
2020-11-27 00:47:33 +0100

    Add route to article

--- api/routes/news.js
+++ api/routes/news.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const News = require('../db/models/News');
 
+const {addNewComment} = require('../db/news');
+const isConnected = require('../middlewares/isConnected');
+const response = require('../utils/response');
+
 router.get('/', async (req,res) => {
     try {
         const news = await News.find();
@@ -19,6 +23,32 @@ router.post('/', async (req, res) => {
     } catch(err) {
         res.status(400).json(err);
     }
+});
+
+router.patch('/comment/:id', isConnected, async function (req, res) {
+    const b = Object.keys(req.body);
+
+    if (b.length > 2)  {
+        return response(res, 422);
+    }
+
+    if (
+        !b.includes('userId') ||
+        !b.includes('content')
+    ) {
+        return response(res, 422);
+    }
+
+    const responseController = await addNewComment(req.params.id, req.body);
+
+    return response(res, responseController.code, {
+        serverHeader: res.locals.data.header || responseController.serverHeader ? {
+            ...res.locals.data.header,
+            ...responseController.serverHeader
+        } : undefined,
+        modifyResponse: responseController.modifyResponse ? responseController.modifyResponse._doc : undefined,
+        token: res.locals.data.forClient ? res.locals.data.forClient.token : undefined
+    });
 });
 
 router.patch('/:id', async (req, res) => {
